b6bac0a6fc8484b71b8a68d05b36a97657bfc35b
Luke Charles <lukecharles222@gmail.com>
2019-01-22 22:52:09 +0000

    test commit

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int choice, num1, num2, total;
+    cout << "Please choose an option:\n1. Addition\n2. Subtraction\n3. Multiplication\n4. Division";
+    cin >> choice;
+    if(choice == 1){
+        cout << "Enter your first number\n";
+        cin >> num1;
+        cout << "Enter your second number\n";
+        cin >> num2;
+        total = num1 + num2;
+        cout << "Your total is " << total << endl;
+    }else if(){
+
+    }
+}
