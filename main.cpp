498f2ba2ee73594a31346f1c3633f4485b484045
Luke Charles <lukecharles222@gmail.com>
2019-01-23 18:17:33 +0000

    square functionality added

--- main.cpp
+++ main.cpp
@@ -5,7 +5,8 @@ using namespace std;
 
 int main() {
     int choice, num1, num2, total;
-    cout << "Please choose an option:\n1. Addition\n2. Subtraction\n3. Multiplication\n4. Division";
+    cout << "Please choose an option:\n1. Addition\n2. Subtraction\n3. Multiplication\n4. Division"
+            "\n5. Square";
     cin >> choice;
     if(choice == 1){
         cout << "Enter your first number\n";
@@ -34,6 +35,20 @@ int main() {
         cout << "Enter your second number\n";
         cin >> num2;
         total = num1 / num2;
+        cout << "Your total is " << total << endl;
+    }else if(choice == 5){
+        total = 0;
+        cout << "Enter your first number\n";
+        cin >> num1;
+        //cout << "Enter your power";
+        //cin >> num2;
+        //int num1tot = num1;
+        //for(int i = 1;i < num2; i++){
+        //    num1tot = num1 * num1;
+        //    total = total + num1tot;
+        //}
+        //cout << "Your total is " << total << endl;
+        total = num1 * num1;
         cout << "Your total is " << total << endl;
     }
 }
